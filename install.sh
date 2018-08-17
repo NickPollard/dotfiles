4ecd015b068a476cf1505e2cb05ad7ef184dbfaf
Nick Pollard <nickedwardpollard@gmail.com>
2018-08-17 10:23:34 -0700

    Update install to backup old files

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+mkdir -p .backup_dotfiles
+mv .vimrc .backup_dotfiles/.
+mv .zshrc .backup_dotfiles/.
+mv .vim .backup_dotfiles/.
+mv .gitconfig .backup_dotfiles/.
 ln -fvs ~/dotfiles/.vimrc .vimrc
 ln -fvs ~/dotfiles/.zshrc .zshrc
 ln -fvs ~/dotfiles/vim .vim
