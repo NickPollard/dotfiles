6f5be36158749a4736ede028f4d10a9e03497584
Nick Pollard <nickpollard@google.com>
2018-08-22 11:31:11 -0700

    [vim] combine nvim and .vim dirs

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@ mkdir -p .backup_dotfiles
 mv .vimrc .backup_dotfiles/.
 mv .zshrc .backup_dotfiles/.
 mv .vim .backup_dotfiles/.
+mv nvim .backup_dotfiles/.
 mv .gitconfig .backup_dotfiles/.
 ln -fvs ~/dotfiles/.vimrc .vimrc
 ln -fvs ~/dotfiles/.zshrc .zshrc
-ln -fvs ~/dotfiles/vim .vim
+ln -fvs ~/dotfiles/vim ~/.vim
+ln -fvs ~/dotfiles/vim ~/nvim
 ln -fvs ~/dotfiles/.gitconfig .gitconfig
 
 touch ~/.zshrc.local
