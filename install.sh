4840e8bb2d446812fad70153c0aab279af9b1a79
Nick Pollard <nickpollard@google.com>
2021-05-12 11:00:29 +0100

    [all] Install init.vim

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
+# Backup existing dotfiles
 mkdir -p .backup_dotfiles
 mv .vimrc .backup_dotfiles/.
 mv .zshrc .backup_dotfiles/.
 mv .vim .backup_dotfiles/.
 mv nvim .backup_dotfiles/.
 mv .gitconfig .backup_dotfiles/.
-ln -fvs ~/.dotfiles/.vimrc .vimrc
-ln -fvs ~/.dotfiles/.zshrc .zshrc
-ln -fvs ~/.dotfiles/vim ~/.vim
-ln -fvs ~/.dotfiles/vim ~/nvim
-ln -fvs ~/.dotfiles/.gitconfig .gitconfig
+mv ~/.config/nvim/init.vim .backup_dotfiles/.
 
+# symlink dotfiles from our repo
+ln -fvs ~/.dotfiles/.vimrc ~/.vimrc
+ln -fvs ~/.dotfiles/.zshrc ~/.zshrc
+ln -fvs ~/.dotfiles/vim ~/.vim
+#ln -fvs ~/.dotfiles/vim ~/nvim
+ln -fvs ~/.dotfiles/.gitconfig .gitconfig
+ln -fvs ~/.dotfiles/vim ~/.config/nvim
+
+# Create an empty .zshrc.local if one doesn't exist
 touch ~/.zshrc.local
 
 # Install vimplug
