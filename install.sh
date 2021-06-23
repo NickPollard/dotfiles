89e8e69c72a97ce3250e781b80130028b3cd4138
Nick Pollard <nickedwardpollard@gmail.com>
2021-06-23 15:29:42 +0100

    [Meta] Tweak install script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-# Backup existing dotfiles
+echo ">>> Backing up existing dotfiles..."
 mkdir -p .backup_dotfiles
-mv .vimrc .backup_dotfiles/.
-mv .zshrc .backup_dotfiles/.
-mv .vim .backup_dotfiles/.
-mv nvim .backup_dotfiles/.
-mv .gitconfig .backup_dotfiles/.
-mv ~/.config/nvim/init.vim .backup_dotfiles/.
+mv -v .vimrc .backup_dotfiles/.
+mv -v .zshrc .backup_dotfiles/.
+mv -v .vim .backup_dotfiles/.
+mv -v nvim .backup_dotfiles/.
+mv -v .gitconfig .backup_dotfiles/.
+mv -v ~/.config/nvim/init.vim .backup_dotfiles/.
 
-# symlink dotfiles from our repo
+echo ">>> Symlink dotfiles from our repo..."
 ln -fvs ~/.dotfiles/.vimrc ~/.vimrc
 ln -fvs ~/.dotfiles/.zshrc ~/.zshrc
 ln -fvs ~/.dotfiles/vim ~/.vim
@@ -15,9 +15,9 @@ ln -fvs ~/.dotfiles/vim ~/.vim
 ln -fvs ~/.dotfiles/.gitconfig .gitconfig
 ln -fvs ~/.dotfiles/vim ~/.config/nvim
 
-# Create an empty .zshrc.local if one doesn't exist
+echo ">>> Create an empty .zshrc.local if one doesn't exist..."
 touch ~/.zshrc.local
 
-# Install vimplug
+echo ">>> Install vimplug..."
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
