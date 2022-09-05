0b5a035849eece2293844c20a467616f33d4c5ee
Nick Pollard <nickedwardpollard@gmail.com>
2022-09-05 17:29:36 +0100

    Add apps.sh

---
+++ apps.sh
@@ -0,0 +1,12 @@
+# hexyl
+echo "Installing 'hexyl' (CLI hex viewer) ..."
+sudo apt-get install hexyl
+# bat
+echo "Installing 'bat' (colorized cat) ..."
+sudo apt-get install bat
+# as-tree
+echo "Instalilng 'as-tree' (tree path formatter)..."
+cargo install -f --git https://github.com/jez/as-tree
+# fd
+echo "Installing 'fd' (find replacement) ..."
+sudo apt-get install fd-find
